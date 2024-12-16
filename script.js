03c5f502d088317618ce4dfa41f1324241603c57
Panta <pantsafa@amazon.ae>
2024-12-17 01:25:17 +0400

    feture add and modified the html for better design

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// function for yoggle menu
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
